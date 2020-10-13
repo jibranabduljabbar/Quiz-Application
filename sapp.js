9b51dab51ede9a57f4df03d8d262a6948afd5023
Jibran Abdul Jabbar <62215537+jibranabduljabbar@users.noreply.github.com>
2020-10-14 01:55:52 +0500

    Add files via upload

---
+++ sapp.js
@@ -0,0 +1,171 @@
+function check(){
+    var score = 0
+    var right_awnser_1 = document.getElementById('q1-a1');
+    var q1_awnser_2 = document.getElementById("q1-a2");
+    var q1_awnser_3 = document.getElementById("q1-a3");
+    var q1_awnser_4 = document.getElementById("q1-a4");
+    if (right_awnser_1.checked == true) {
+        score++
+        alert('Q1 Awnser Right!')
+    }
+    else{
+        alert('Q1 Awnser Wrong!')
+    }
+
+
+
+
+
+
+
+    var right_awnser_2 = document.getElementById("q2-a1");
+    var q2_awnser_2 = document.getElementById("q2-a1");
+    var q3_awnser_2 = document.getElementById("q2-a1");
+    var q4_awnser_2 = document.getElementById("q2-a1");
+   if (right_awnser_2.checked == true) {
+       score++
+       alert("Q2 Awnser Right!")
+   }
+   else{
+   alert("Q2 Awnser Wrong!")
+   }
+
+
+
+
+
+
+
+   var right_awnser_3 = document.getElementById("q3-a1");
+    var q2_awnser_3 = document.getElementById("q3-a2");
+    var q3_awnser_3= document.getElementById("q3-a3");
+    var q4_awnser_3 = document.getElementById("q3-a4");
+   if (right_awnser_3.checked == true) {
+       score++
+       alert("Q3 Awnser Right!")
+   }
+   else{
+       alert("Q3 Awnser Wrong!")
+   }
+   
+
+
+
+
+   var right_awnser_4 = document.getElementById("q4-a1");
+    var q2_awnser_4 = document.getElementById("q4-a2");
+    var q3_awnser_4= document.getElementById("q4-a3");
+    var q4_awnser_4 = document.getElementById("q4-a4");
+   if (right_awnser_4.checked == true) {
+       score++
+       alert("Q4 Awnser Right!")
+   }
+   else{
+       alert("Q4 Awnser Wrong!")
+   }
+
+
+
+
+
+   var right_awnser_5 = document.getElementById("q5-a1");
+    var q2_awnser_5 = document.getElementById("q5-a2");
+    var q3_awnser_5= document.getElementById("q5-a3");
+    var q4_awnser_5 = document.getElementById("q5-a4");
+   if (right_awnser_5.checked == true) {
+       score++
+       alert("Q5 Awnser Right!")
+   }
+   else{
+       alert("Q5 Awnser Wrong!")
+   }
+
+
+
+
+
+   var right_awnser_6 = document.getElementById("q6-a1");
+    var q2_awnser_6 = document.getElementById("q6-a2");
+    var q3_awnser_6= document.getElementById("q6-a3");
+    var q4_awnser_6 = document.getElementById("q6-a4");
+   if (right_awnser_6.checked == true) {
+       score++
+       alert("Q6 Awnser Right!")
+   }
+   else{
+       alert("Q6 Awnser Wrong!")
+   }
+
+
+
+
+
+   var right_awnser_7 = document.getElementById("q7-a1");
+    var q2_awnser_7 = document.getElementById("q7-a2");
+    var q3_awnser_7= document.getElementById("q7-a3");
+    var q4_awnser_7 = document.getElementById("q7-a4");
+   if (right_awnser_7.checked == true) {
+       score++
+       alert("Q7 Awnser Right!")
+   }
+   else{
+       alert("Q7 Awnser Wrong!")
+   }
+
+
+
+
+
+
+   var right_awnser_8 = document.getElementById("q8-a1");
+    var q2_awnser_8 = document.getElementById("q8-a2");
+    var q3_awnser_8= document.getElementById("q8-a3");
+    var q4_awnser_8 = document.getElementById("q8-a4");
+   if (right_awnser_8.checked == true) {
+       score++
+       alert("Q8 Awnser Right!")
+   }
+       else{
+        alert("Q8 Awnser Wrong!")
+       }
+       
+       
+
+
+
+
+    var right_awnser_9 = document.getElementById("q9-a1");
+    var q2_awnser_9 = document.getElementById("q9-a2");
+    var q3_awnser_9= document.getElementById("q9-a3");
+    var q4_awnser_9 = document.getElementById("q9-a4");
+    if (right_awnser_9.checked == true) {
+          score++
+          alert("Q9 Awnser Right!")
+      }
+          else{
+           alert("Q9 Awnser Wrong!")
+          }
+
+
+
+
+
+
+
+          
+    var right_awnser_10 = document.getElementById("q10-a1");
+    var q2_awnser_10 = document.getElementById("q10-a2");
+    var q3_awnser_10 = document.getElementById("q10-a3");
+    var q4_awnser_10 = document.getElementById("q10-a4");
+    if (right_awnser_10.checked == true) {
+          score++
+          alert("Q10 Awnser Right!")
+      }
+          else{
+           alert("Q10 Awnser Wrong!")
+          }
+}
+
+
+
+    
